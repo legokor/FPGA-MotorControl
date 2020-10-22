74c4d9e3a44872de0b6d20988be4773d159ae2dd
GaryBlackbourne <gergely.koloszar@gmail.com>
2020-10-22 14:28:12 +0200

    Added pwm_generator.v source

--- sources/pwm_generator.v
+++ sources/pwm_generator.v
@@ -3,11 +3,71 @@
 module pwm_generator #(
     parameter CNTR_BITS = 16
 )(
-    input clk,
-    input [CNTR_BITS-1 : 0] period,
-    input [CNTR_BITS-1 : 0] compare,
-
+    input clk,  // órajel
+    input en,   // enable
+    input rst,  // reset
+    input [CNTR_BITS - 1:0] cmp,
+    input [CNTR_BITS - 1:0] period, 
     output pwm_out
 );
+
+reg [CNTR_BITS-1 : 0] cnt;
+reg out;
+
+always@ (posedge clk) begin
+
+if(rst) begin
+
+    cnt <= 0;
+    
+end else begin    
+       
+    if(en) begin
+        
+        if(cnt == period) begin
+            
+            cnt <= 0;
+            
+        end else begin
+          
+            cnt <= cnt + 1;
+            
+        end
+    
+    end
+
+end
+
+end
+
+
+
+always@ (posedge clk) begin
+
+if(rst) begin
+
+    out <= 0;
+    
+end else begin
+
+    if(en) begin
+       
+        if(cmp[CNTR_BITS - 1:0] > cnt[CNTR_BITS - 1:0]) begin
+        
+            out <= 1;
+        
+        end else begin
+        
+            out <= 0;
+        
+        end
+    
+    end
+    
+end
+
+end
+
+assign pwm_out = out & en;
 
 endmodule
